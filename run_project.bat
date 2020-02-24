fb91e5be76f0a7818b4187454fb253b3b8a404d8
Elias <elias2603@hotmail.com>
2020-02-25 00:56:40 +0100

    Adding the runfiles for win and lx

---
+++ run_project.bat
@@ -0,0 +1,10 @@
+@echo OFF
+ECHO Welcome to the McLaren Applied Technologies Fan Engagement Application! Wait a little while we set up all the dependencies
+docker-compose pull
+pip3 install pipenv
+cd apps\fan_engagement
+pipenv install
+docker-compose stop
+docker-compose up -d
+ECHO All set! Launching application, now you can go to localhost:8084 and start watching the cars go around :)
+pipenv run py main.py
