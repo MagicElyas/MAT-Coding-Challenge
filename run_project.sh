a68f7de0e10c63760b472c732dce931c76223fdc
Elias <MagicElyas@users.noreply.github.com>
2020-02-25 12:57:10 +0100

    Update run_project.sh

--- run_project.sh
+++ run_project.sh
@@ -5,5 +5,5 @@ cd apps/fan_engagement
 pipenv install
 docker-compose stop
 docker-compose up -d
-echo All set! Launching application, now you can go to localhost:8084 and start watching the cars go around :)
+echo All set! Launching application, now you can go to localhost:8084 and start watching the cars go around 
 pipenv run python3 main.py
